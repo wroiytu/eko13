aa09d11fbf55350b049fdb9dc36a6be8a5d8cdd9
wroiytu <70208062+wroiytu@users.noreply.github.com>
2020-08-25 20:57:27 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -42,7 +42,7 @@ if version_info[0] < 3 or version_info[1] < 8:
 
 # Check if the config was edited by using the already used variable.
 # Basically, its the 'virginity check' for the config file ;)
-CONFIG_CHECK = os.environ.get(**,none)
+CONFIG_CHECK = os.environ.get(**, none)
 
 if CONFIG_CHECK:
     LOGS.info(
